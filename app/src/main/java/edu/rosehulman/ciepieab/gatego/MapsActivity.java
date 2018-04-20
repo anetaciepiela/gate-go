abebfdecf11de34ce32fd67bf91f727ea2e5679b
ciepieab <37120131+ciepieab@users.noreply.github.com>
2018-04-20 18:11:48 -0400

    pair programming with michelle, worked on the add airport dialog - using calendar

--- app/src/main/java/edu/rosehulman/ciepieab/gatego/MapsActivity.java
+++ app/src/main/java/edu/rosehulman/ciepieab/gatego/MapsActivity.java
@@ -6,9 +6,12 @@ import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,13 +20,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
+
+import static java.lang.Math.abs;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private EditText addAirportEditText;
+    private Calendar mCalendar;
+    private DateFormat mTodayTomorrowFormatter;
+    private DateFormat mOtherDayFormatter;
+
+
+
+
     //private ArrayList mAirports;
 
     @Override
@@ -36,6 +50,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         //mAirports = new ArrayList();
+        mCalendar = Calendar.getInstance();
+        mTodayTomorrowFormatter = new SimpleDateFormat("MM/dd/yy");
+        mOtherDayFormatter = new SimpleDateFormat("E-MM/dd/yy");
         addAirportEditText = findViewById(R.id.add_airport_editText);
 
         addAirportEditText.setOnClickListener(new View.OnClickListener() {
@@ -52,16 +69,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void showAddAirportDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.dialog_add_airport, null, false);
+        //TODO DATE TYPE
         //Capture widgets
         final EditText airportNameEditText = view.findViewById(R.id.airport_name_editText);
         final EditText startGateEditText = view.findViewById(R.id.start_gate_editText);
         final EditText destGateNameEditText = view.findViewById(R.id.dest_gate_editText);
-        final CalendarView calendarView = view.findViewById(R.id.calendar_view);
-        final GregorianCalendar calendar = new GregorianCalendar();
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        final LinearLayout dateLayout = view.findViewById(R.id.date_layout);
+        final TextView dateTextView = view.findViewById(R.id.date_text_view);
+        final String todaysDate = mTodayTomorrowFormatter.format(mCalendar.getTime());
+        String defaultDate = getResources().getQuantityString(R.plurals.date_format, 1,todaysDate);
+        dateTextView.setText(defaultDate);
+        dateLayout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                calendar.set(year, month, dayOfMonth);
+            public void onClick(View v) {
+                showCalendarDialog(todaysDate, dateTextView);
+                //dateTextView.setText(showCalendarDialog(todaysDate));
+                //String selectedFormattedDate = showCalendarDialog(todaysDate);
+                //dateTextView.setText(selectedFormattedDate);
             }
         });
         builder.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
@@ -72,6 +96,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
         builder.setNegativeButton(android.R.string.cancel, null);
         builder.setView(view);
+        builder.create().show();
+    }
+
+    @NonNull
+    private String getFormattedDate(String selectedDate, String todaysDate) {
+        Log.d("Day", String.valueOf(Integer.parseInt(selectedDate.substring(3, 4))));
+        int dateDifference = abs(Integer.parseInt(todaysDate.substring(3, 5)) - Integer.parseInt(selectedDate.substring(3, 5)));
+        String selectedDateFormatted = "";
+        if(dateDifference == 0) {
+            selectedDateFormatted = getResources().getQuantityString(R.plurals.date_format, 1,selectedDate);
+        }
+        else if(dateDifference == 1) {
+            //TODO: FORMAT
+            selectedDateFormatted = getResources().getQuantityString(R.plurals.date_format, 2,selectedDate);
+        }
+        else {
+            //TODO FORMAT
+            selectedDateFormatted = getResources().getQuantityString(R.plurals.date_format, 3,selectedDate);
+        }
+        return selectedDateFormatted;
+    }
+
+    private void showCalendarDialog(final String todaysDate, final TextView dateTextView) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_calendar, null, false);
+        final CalendarView calendarView = view.findViewById(R.id.calendar_view);
+        final GregorianCalendar calendar = new GregorianCalendar();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                calendar.set(year, month, dayOfMonth);
+            }
+        });
+        builder.setView(view);
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("Correct DATE", getFormattedDate(mTodayTomorrowFormatter.format(calendar.getTime()), todaysDate));
+                dateTextView.setText(getFormattedDate(mTodayTomorrowFormatter.format(calendar.getTime()), todaysDate));
+            }
+        });
+
         builder.create().show();
     }
 
